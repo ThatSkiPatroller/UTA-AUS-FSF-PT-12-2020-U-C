aaae473d5059f62e10b7f6cc7ea6b0263b81751b
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-02-24 19:16:31 -0600

    updated activities 11 - 14

---
+++ 09-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
@@ -0,0 +1,7 @@
+console.log(process.argv);
+
+if (process.argv[2] === process.argv[3]) {
+    console.log("ture");
+} else {
+    console.log("false");
+};
--- 09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
+++ 09-NodeJS/01-Activities/14-Stu_appendFile/Unsolved/index.js
@@ -1,8 +1,12 @@
 // TODO: What are we importing?
+// We are importing the fs module that enables us to interact with the file system
 const fs = require('fs');
 
 // TODO: Add comments to explain each of the three arguments of appendFile()
+// The first argument is a message and the second is the data to append
 fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
   // TODO: Describe how this ternary operator works
+  // takes a condition followed by question mark and line to execute if the 
+  // condition is truthy, then colon and another line to execute if condition is false
   err ? console.error(err) : console.log('Commit logged!')
 );
